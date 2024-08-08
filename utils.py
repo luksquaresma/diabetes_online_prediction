fd741ce64fcda672f205dc06cab34909943270f7
Lucas Quaresma <lucas.quaresma1@gmail.com>
2024-08-08 18:07:11 -0300

    updates

--- utils.py
+++ utils.py
@@ -3,11 +3,11 @@ import pandas as pd
 
 def balance_bin(df, cols):
     for col in cols:
-        x = len(df[df[col] == 0])/len(df[df[col] == 1])
+        x = len(df[df[col] == False])/len(df[df[col] == True])
         alpha = int(np.ceil(x)) if x >= 0 else int(np.ceil(1/x))
         
         print((col, alpha))
 
-        # for _ in range(alpha - 1):
-            # df = pd.concat([df, df[df[col] == 1]], ignore_index=True)
+        for _ in range(alpha - 1):
+            df = pd.concat([df, df[df[col] == True if x >= 0 else False]], ignore_index=True)
     return df
